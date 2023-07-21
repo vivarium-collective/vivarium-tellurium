ee79daa704b1bd6ebefdbc495b6035d9ab338717
alex patrie <alexanderpatrie@gmail.com>
2023-07-21 17:07:14 -0400

    feat: implemented test antimony instance

--- vivarium_tellurium/processes/tellurium_process.py
+++ vivarium_tellurium/processes/tellurium_process.py
@@ -1,4 +1,6 @@
 import json 
+from typing import Union
+import importlib
 import tellurium as te 
 from vivarium.core.process import Process
 from vivarium.core.engine import Engine, pp
@@ -9,8 +11,8 @@ class TelluriumProcess(Process):
 
     # declare default parameters as class variables
     defaults = {
+        'api': 'tellurium',
         'model_path': '',
-        'uptake_rate': 0.1,
     }
 
     def __init__(self, parameters=None):
@@ -72,9 +74,106 @@ class TelluriumProcess(Process):
         return update
 
 
+class AntimonyTelluriumProcess(TelluriumProcess):
+    
+    defaults = {
+        'api': 'tellurium',
+        'api_imports': ('loada'),
+        'antimony_string': '',
+    }
+    
+    def __init__(self, parameters=None):
+        #instantiate the parent
+        super().__init__(parameters)
+        
+        #get the tellurium package as an object
+        tellurium = importlib.import_module(self.parameters['api'])
+        
+        #traverse the parameters to extract the package imports needed
+        for content in self.parameters['api_imports']:
+            self.__setattr__(content, getattr(tellurium, content))
+            
+            
+    def next_update(self, interval, states, n_timesteps=100, antimony_string=None):
+        # get the states from the tellurium model
+        internal_A = states['internal']['A']
+        external_A = states['external']['A']
 
+        # run tellurium here
+        antimony_string = antimony_string or self.parameters['antimony_string']
+        internal_update = self.loada(antimony_string).simulate(0, interval, n_timesteps)
+        external_update = -1 * internal_update
+
+        # get the results from tellurium
+        update = {
+            'internal': {
+                'A': internal_update},
+            'external': {
+                'A': external_update}
+        }
+
+        # return an update that mirrors the ports structure
+        return update
+            
+
+
+def test_process(process: Union[TelluriumProcess, AntimonyTelluriumProcess], 
+                 process_name: str,
+                 initial_state: dict, 
+                 total_time: float,
+                 config: dict = {}):
+    proc = process(config)
+    ports = proc.ports_schema()
+    sim = Engine(
+        processes={process_name: proc},
+        topology={process_name: {port_id: (port_id,) for port_id in ports.keys()}},
+        initial_state=initial_state
+    )
+    sim.update(total_time)
+    return sim.emitter.get_data()
+    
 
 def test_tellurium():
+    config = {}
+    process = TelluriumProcess(config)
+    name = 'tellurium'
+    initial_state = {
+        'internal': {
+            'A': 0.0
+        },
+        'external': {
+            'A': 1.0
+        },
+    }
+    data = test_process(process, name, initial_state, 10.0, config)
+    pp(data)
+    
+def test_antimony():
+    config = {}
+    process = AntimonyTelluriumProcess(config)
+    process.parameters['antimony_string'] = 'S1 -> S2; k1*S1; k1 = 0.1; S1 = 10'
+    config['antimony_string'] = 'S1 -> S2; k1*S1; k1 = 0.1; S1 = 10'
+    name = 'antimony'
+    initial_state = {
+        'internal': {
+            'A': 0.0
+        },
+        'external': {
+            'A': 1.0
+        },
+    }
+    ports = process.ports_schema()
+    sim = Engine(
+        processes={name: process},
+        topology={name: {port_id: (port_id,) for port_id in ports.keys()}},
+        initial_state=initial_state
+    )
+    sim.update(10.0)
+    data = sim.emitter.get_data()
+    pp(data)
+    
+
+def __test_tellurium():
     totaltime = 10.0
 
     # initialize the process
@@ -112,4 +211,4 @@ def test_tellurium():
 
 
 if __name__ == '__main__':
-    test_tellurium()
+    test_antimony()
