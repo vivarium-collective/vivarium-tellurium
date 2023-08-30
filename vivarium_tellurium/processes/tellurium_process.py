40c126384ffe58da08431844413d63702e1fea38
Eran Agmon <eranagmon@lt-0921404.uchc.edu>
2023-08-30 11:09:35 -0400

    get vivarium-tellurium working

--- vivarium_tellurium/processes/tellurium_process.py
+++ vivarium_tellurium/processes/tellurium_process.py
@@ -42,100 +42,115 @@ class TelluriumProcess(Process):
             self.simulator = te.loadSBMLModel(self.parameters['sbml_model_path'])
 
         # extract the variables 
-        self.species = self.simulator.get_species()  # PLACEHOLDER!!!!!!!!
+        self.floating_species_list = self.simulator.getFloatingSpeciesIds()
+        self.boundary_species_list = self.simulator.getBoundarySpeciesIds()
+        self.floating_species_initial = self.simulator.getFloatingSpeciesConcentrations()
+        self.boundary_species_initial = self.simulator.getBoundarySpeciesConcentrations()
+        self.reaction_list = self.simulator.getReactionIds()
+
+    def initial_state(self, config=None):
+        floating_species_dict = dict(zip(self.floating_species_list, self.floating_species_initial))
+        boundary_species_dict = dict(zip(self.boundary_species_list, self.boundary_species_initial))
+        return {
+            'floating_species': floating_species_dict,
+            'boundary_species': boundary_species_dict,
+        }
 
     def ports_schema(self):
         # TODO -- set the ports/variables according to self.config assignments. Similar to viv-biosimul
         return {
-            'species': {
+            'floating_species': {
                 species_id: {
                     '_default': 1.0,
                     '_updater': 'set',
                     '_emit': True,
-                } for species_id in self.config['exposed_species']
-            }
+                } for species_id in self.floating_species_list
+            },
+            'boundary_species': {
+                species_id: {
+                    '_default': 1.0,
+                    '_updater': 'set',
+                    '_emit': True,
+                } for species_id in self.boundary_species_list
+            },
+            'reactions': {
+                '_default': self.reaction_list},
         }
 
     def next_update(self, interval, states):
 
         # set the states in tellurium according to what is passing in states
-        for species_id, value in states['species'].items():
-            self.tellurium_object.set_species(species_id, value)
+        for species_id, value in states['floating_species'].items():
+            self.simulator.setValue(species_id, value)
+        for species_id, value in states['boundary_species'].items():
+            self.simulator.setValue(species_id, value)
 
         # run the simulation
-        self.tellurium_object.simulate(0, interval, 1)
+        self.simulator.simulate(0, interval, 2)
 
         # extract the results. TODO -- get the final values of the self.config['exposed_species'] and put them in the update
-        update = {'species': {}}
-        results = self.tellurium_object.get_data()
+        final_concentrations = self.simulator.getFloatingSpeciesConcentrations()
 
-        return update
+
+        floating_species_dict = dict(zip(self.floating_species_list, final_concentrations))
+        return {
+            'floating_species': floating_species_dict
+        }
 
 
 # functions to configure and run the process
 def test_tellurium_process():
-    sbml_model_path = 'vivarium_tellurium/models/Caravagna2010.xml'
+    total_time = 5
+    sbml_model_path = 'vivarium_tellurium/models/BIOMD0000000061_url.xml'  # Caravagna2010.xml'
 
-    # 1. Declare the initial state, mirroring the ports structure. May be passed as a parsable argument.
-    initial_state = {
-        'internal': {
-            'A': 0.0
-        },
-        'external': {
-            'A': 1.0
-        },
-    }
-    
-    # 2. Create the simulation run parameters for the simulator
+    # Create the simulation run parameters for the simulator
     config = {
         'sbml_model_path': sbml_model_path,
     }
             
-    # 3. Initialize the process by passing in a config dict
+    # Initialize the process by passing in a config dict
     template_process = TelluriumProcess(config)
-    
-    # 4. Get the ports for the process
-    template_process_ports = template_process.ports_schema()
 
-    # 4a. view the ports:
-    print(f'PORTS FOR {pp(template_process)}: {pp(template_process_ports)}')
-    
-    # 5. Feed the Simulator Process instance you just created to the vivarium Engine
+    # Get the ports for the process
+    process_ports = template_process.ports_schema()
+
+    # Get the initial state
+    initial_state = template_process.initial_state()
+
+    # Feed the Simulator Process you just created to the vivarium Engine
     sim = Engine(
         processes={
             'template': template_process,
         },
         topology={
             'template': {
-                port_id: (port_id,) for port_id in template_process_ports.keys()
+                port_id: (port_id,) for port_id in process_ports.keys()
             },
         },
         initial_state=initial_state
     )
     
-    # 6. Call update with that sim object, which calls the next_update method in the implementation you created above using total_time
-    total_time = 3
+    # Call update with that sim object, which calls the next_update method in the implementation you created above using total_time
     sim.update(
         interval=total_time
     )
     
-    # 7. Get the data which is emitted from the sim object.
+    # Get the data which is emitted from the sim object.
     data = sim.emitter.get_timeseries()
     
-    # 7a. Observe the data which is return from running the process:
+    # Observe the data which is return from running the process:
     print(f'RESULTS: {pp(data)}')
-    return data
 
 
-def test_load_from_antimony():
-    # 1. Adding an antimony string to your config dict will allow for "antimony mode" to be turned on.
-    config = {
-        'antimony_string': 'S1 -> S2; k1*S1'
-    }
-
-    # 2. Initialize the process by passing in a config dict
-    antimony_process = TelluriumProcess(config)
-    print(antimony_process.simulator)
+# def test_load_from_antimony():
+#     # 1. Adding an antimony string to your config dict will allow for "antimony mode" to be turned on.
+#     config = {
+#         'antimony_string': 'S1 -> S2; k1*S1'
+#     }
+#
+#     # 2. Initialize the process by passing in a config dict
+#     antimony_process = TelluriumProcess(config)
+#     print(antimony_process.simulator)
 
 
 # run module with python vivarium_tellurium/processes/tellurium_process.py
